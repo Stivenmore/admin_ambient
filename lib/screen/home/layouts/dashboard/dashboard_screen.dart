aa67943cee8d755d908f03dce6192a27801f072a
Noah <Ingenierostiven@outlook.es>
2022-11-14 16:38:55 -0500

    save navigation table and mobile

--- lib/screen/home/layouts/dashboard/dashboard_screen.dart
+++ lib/screen/home/layouts/dashboard/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:admin_ambient/domain/logic/general/general_cubit.dart';
+import 'package:admin_ambient/domain/logic/podcast/podcast_cubit.dart';
 import 'package:admin_ambient/domain/logic/signIn_signUp/sign_in_and_sign_up_cubit.dart';
 import 'package:admin_ambient/screen/home/layouts/dashboard/components/analytics_components.dart';
 import 'package:admin_ambient/screen/home/layouts/dashboard/layouts/detail_user/layouts/detailuser_screen.dart';
@@ -47,7 +48,9 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
       ClipRRect(
         borderRadius: BorderRadius.circular(26),
         child: Container(
-          height: responsive.height - responsive.st(8),
+          height: imageCalculateWidthHome(responsive) != DisplayType.web
+              ? responsive.height
+              : responsive.height - responsive.st(8),
           width: imageCalculateWidthHome(responsive) == DisplayType.web
               ? responsive.wp(75)
               : responsive.width,
@@ -55,12 +58,79 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
               color: UniCodes.ligthgrey,
               borderRadius: BorderRadius.circular(26)),
           child: AnimatedSwitcher(
-            duration: const Duration(milliseconds: 200),
-            key: ValueKey(generalCubit.state.currentPageDashboard),
-            child: generalCubit.state.globalcurrentpage == 0
-                ? screens[generalCubit.state.currentPageDashboard]!
-                : globalScreen[generalCubit.state.globalcurrentpage],
-          ),
+              duration: const Duration(milliseconds: 200),
+              key: ValueKey(generalCubit.state.currentPageDashboard),
+              child: Stack(
+                children: [
+                  Padding(
+                    padding:
+                        imageCalculateWidthHome(responsive) != DisplayType.web
+                            ? const EdgeInsets.all(30)
+                            : const EdgeInsets.all(0),
+                    child: generalCubit.state.globalcurrentpage == 0
+                        ? screens[generalCubit.state.currentPageDashboard]!
+                        : globalScreen[generalCubit.state.globalcurrentpage]!,
+                  ),
+                  if (imageCalculateWidthHome(responsive) != DisplayType.web)
+                    Builder(builder: (context) {
+                      final generalcubit = context.read<GeneralCubit>();
+                      final value2 = context.select<GeneralCubit, int>(
+                          (value2) => value2.state.globalcurrentpage);
+                      final statePodcast =
+                          context.select<PodcastCubit, PodCastEnum>(
+                              (value) => value.state.podCastEnum);
+                      return SizedBox(
+                        height: 30,
+                        width: responsive.width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  if (value2 != 0 &&
+                                      statePodcast != PodCastEnum.loading) {
+                                    generalcubit.changeGlobalCurrentPage(0);
+                                  }
+                                },
+                                icon: Icon(Icons.home,
+                                    color: value2 == 0
+                                        ? UniCodes.orangeperformance2
+                                        : UniCodes.gray2)),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            IconButton(
+                                onPressed: () {
+                                  if (value2 != 1 &&
+                                      statePodcast != PodCastEnum.loading) {
+                                    generalcubit.changeGlobalCurrentPage(1);
+                                  }
+                                },
+                                icon: Icon(Icons.notifications,
+                                    color: value2 == 1
+                                        ? UniCodes.orangeperformance2
+                                        : UniCodes.gray2)),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            IconButton(
+                                onPressed: () {
+                                  if (value2 != 2 &&
+                                      statePodcast != PodCastEnum.loading) {
+                                    generalcubit.changeGlobalCurrentPage(2);
+                                  }
+                                },
+                                icon: Icon(Icons.settings_voice_rounded,
+                                    color: value2 == 2
+                                        ? UniCodes.orangeperformance2
+                                        : UniCodes.gray2))
+                          ],
+                        ),
+                      );
+                    }),
+                ],
+              )),
         ),
       ),
     ]);
